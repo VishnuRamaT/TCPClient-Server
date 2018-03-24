9fda53ebbe560d96eeb8206760e028ab9b81257b
locslr <sreelakhmir1996@gmail.com>
2018-03-24 11:19:32 +0530

    client modified

--- Client.java
+++ Client.java
@@ -4,5 +4,17 @@ public class Client
 {
   public static void main(String[] args) throws Exception
   {
+
+Socket sock = new Socket("127.0.0.1", 3000);
+                               // reading from keyboard (keyRead object)
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+                              // sending to client (pwrite object)
+     OutputStream ostream = sock.getOutputStream(); 
+     PrintWriter pwrite = new PrintWriter(ostream, true);
+ 
+                              // receiving from server ( receiveRead  object)
+     InputStream istream = sock.getInputStream();
+     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
 }
 }
