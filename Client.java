b753193a7bc418118fd45fafb2f9e584020f1baa
locslr <sreelakhmir1996@gmail.com>
2018-03-24 11:20:44 +0530

    client pgm final

--- Client.java
+++ Client.java
@@ -16,5 +16,18 @@ Socket sock = new Socket("127.0.0.1", 3000);
      InputStream istream = sock.getInputStream();
      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
  
+
+ String receiveMessage, sendMessage;               
+     while(true)
+     {
+        sendMessage = keyRead.readLine();  // keyboard reading
+        pwrite.println(sendMessage);       // sending to server
+        pwrite.flush();                    // flush the data
+        if((receiveMessage = receiveRead.readLine()) != null) //receive from server
+        {
+            System.out.println(receiveMessage); // displaying at DOS prompt
+        }         
+      }  
+
 }
 }
