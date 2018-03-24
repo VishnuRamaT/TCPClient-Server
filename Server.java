c54dd33d7b3c2bdcfdcd950f3740c04c875e2449
Vishnu <vichuzzzzs@gmail.com>
2018-03-24 10:55:35 +0530

    third commit

--- Server.java
+++ Server.java
@@ -4,5 +4,16 @@ public class Server
 {
   public static void main(String[] args) throws Exception
 {
-}
+
+
+
+	ServerSocket sersock = new ServerSocket(3000);
+        System.out.println("Server  ready for chatting");
+	Socket sock = sersock.accept( ); 
+// reading from keyboard (keyRead object)
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      // sending to client (pwrite object)
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwrite = new PrintWriter(ostream, true);
+ }
 }
